dd99a06d690a0711946e5a528ac6a6cceb080789
abuq <TheAbukPL@wp.pl>
2021-09-30 23:39:21 +0200

    added links to modules

--- lesser.py
+++ lesser.py
@@ -2,8 +2,8 @@
 
 version = 'v2.0'
 
-import docx2txt#zamienia docx na txt
-from PIL import Image#operacje na zdjęciach
+import docx2txt#https://pypi.org/project/docx2txt/
+from PIL import Image#https://pypi.org/project/Pillow/
 import os
 
 
