7817bc6ff04d0dedf932808236ef7cad781292d8
Yellow <yellowbuq@gmail.com>
2022-05-31 03:00:13 +0200

    integrated with the docx2txt library

--- lesser.py
+++ lesser.py
@@ -1,10 +1,90 @@
 #!/usr/bin/env python3
 
-version = '3.1'
+version = '3.2'
 
-import docx2txt#https://pypi.org/project/docx2txt/
 from PIL import Image#https://pypi.org/project/image/
 import os
+
+
+#docx2txt Source: https://pypi.org/project/docx2txt/
+import re
+import xml.etree.ElementTree as ET
+import zipfile
+import os
+
+nsmap = {'w': 'http://schemas.openxmlformats.org/wordprocessingml/2006/main'}
+
+def qn(tag):
+    """
+    Stands for 'qualified name', a utility function to turn a namespace
+    prefixed tag name into a Clark-notation qualified tag name for lxml. For
+    example, ``qn('p:cSld')`` returns ``'{http://schemas.../main}cSld'``.
+    Source: https://github.com/python-openxml/python-docx/
+    """
+    prefix, tagroot = tag.split(':')
+    uri = nsmap[prefix]
+    return '{{{}}}{}'.format(uri, tagroot)
+
+
+def xml2text(xml):
+    """
+    A string representing the textual content of this run, with content
+    child elements like ``<w:tab/>`` translated to their Python
+    equivalent.
+    Adapted from: https://github.com/python-openxml/python-docx/
+    """
+    text = u''
+    root = ET.fromstring(xml)
+    for child in root.iter():
+        if child.tag == qn('w:t'):
+            t_text = child.text
+            text += t_text if t_text is not None else ''
+        elif child.tag == qn('w:tab'):
+            text += '\t'
+        elif child.tag in (qn('w:br'), qn('w:cr')):
+            text += '\n'
+        elif child.tag == qn("w:p"):
+            text += '\n\n'
+    return text
+
+
+def process(docx, img_dir=None):
+    text = u''
+
+    # unzip the docx in memory
+    zipf = zipfile.ZipFile(docx)
+    filelist = zipf.namelist()
+
+    # get header text
+    # there can be 3 header files in the zip
+    header_xmls = 'word/header[0-9]*.xml'
+    for fname in filelist:
+        if re.match(header_xmls, fname):
+            text += xml2text(zipf.read(fname))
+
+    # get main text
+    doc_xml = 'word/document.xml'
+    text += xml2text(zipf.read(doc_xml))
+
+    # get footer text
+    # there can be 3 footer files in the zip
+    footer_xmls = 'word/footer[0-9]*.xml'
+    for fname in filelist:
+        if re.match(footer_xmls, fname):
+            text += xml2text(zipf.read(fname))
+
+    if img_dir is not None:
+        # extract images
+        for fname in filelist:
+            _, extension = os.path.splitext(fname)
+            if extension in [".jpg", ".jpeg", ".png", ".bmp"]:
+                dst_fname = os.path.join(img_dir, os.path.basename(fname))
+                with open(dst_fname, "wb") as dst_f:
+                    dst_f.write(zipf.read(fname))
+
+    zipf.close()
+    return text.strip()
+
 
 
 def felieton(filenamedocx):
@@ -20,7 +100,7 @@ def felieton(filenamedocx):
 
 	try:
 
-		for i,line in enumerate(docx2txt.process(filenamedocx,'r').strip().split('\n')):
+		for i,line in enumerate(process(filenamedocx,'r').strip().split('\n')):
 			if i == 0:
 				tytuł += line+'\n'#Na początku wydzielam tytuł
 				continue
@@ -142,7 +222,7 @@ def poezja(filenamedocx):
 
 	try:
 
-		for i,line in enumerate(docx2txt.process(filenamedocx,'r').strip().split('\n')):#args.filenamedocx
+		for i,line in enumerate(process(filenamedocx,'r').strip().split('\n')):#args.filenamedocx
 
 			if i == 0:#Wykonuje sie tylko raz
 				Tytuł += line+'\n'
